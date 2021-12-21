fa806c69cd4832dbe2d2d87c740994ef855c8132
Ixales <92451841+Ixales@users.noreply.github.com>
2021-12-21 03:02:59 +0100

    P5

--- src/Tableau1.js
+++ src/Tableau1.js
@@ -1,33 +1,48 @@
-class Tableau1 {
+class Tableau1 extends Phaser.Scene {
     /**
      * Précharge les assets
      */
     preload() {
         this.load.image('tiplouf', 'img/tiplouf.jpg');
+        this.load.image('pokedex', 'img/whos_that_pokemon.png');
+
     }
 
-    create(){
+    create() {
 
-        let tiplouf=this.add.image(-300,60, 'tiplouf').setOrigin(0,0);
-        this.groundContainer.add(vine203);
-        vine203.scale=0.7
+        this.add.image(0,0, 'pokedex').setOrigin(0,0);
+
+        this.Tiplouf=this.add.image(300,300,'tiplouf').setOrigin(0,0);
+        this.Tiplouf.alpha=0;
+        this.initKeyboard();
+
     }
 
 
+        initKeyboard(){
+            let me=this;
+            this.input.keyboard.on('keydown', function(kevent)
+            {
+                switch (kevent.keyCode)
+                {
+                    case Phaser.Input.Keyboard.KeyCodes.A:
 
-    initKeyboard(){
-        let me=this;
-        this.input.keyboard.on('keydown'), function(kevent) {
-            switch (kevent.keyCode) {
-                case Phaser.Input.Keyboard.KeyCodes.A:
+                        if (me.Tiplouf.alpha === 0) {
+                            me.Tiplouf.setAlpha(1)
+                        } else {
+                            me.Tiplouf.setAlpha(0)
+                        }
+                        break;
+                }
+            });
+            this.input.keyboard.on('keyup', function(kevent)
+            {
+                switch (kevent.keyCode)
+                {
 
-                    if (me.filterNeige.alpha === 0) {
-                        me.filterNeige.setAlpha(1)
-                    } else {
-                        me.filterNeige.setAlpha(0)
-                    }
-                    break;
-            }
+                }
+            });
+
         }
-    }
+
 }
---
+++ src/main1.js
@@ -0,0 +1,15 @@
+let gameConfig = {
+    type: Phaser.AUTO,
+    width: 1280,
+    height: 720,
+    backgroundColor: '#ffffff',
+    parent: 'game',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },
+    scene: new Tableau1()
+};
+let game = new Phaser.Game(gameConfig);
